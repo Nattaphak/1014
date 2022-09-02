ec2de02dcb0d87a39371e9bd87c42face6f36acc
Nattaphak Udomwasurat <nattaphak.udom@kmutt.ac.th>
2022-09-02 18:38:50 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] number = new int[3];
+        int sum = 0;
+
+        for(int i = 0 ; i < 3 ; i ++)
+        {
+            Console.WriteLine("Please input number {0} : ",i+1);
+            int.TryParse(Console.ReadLine(), out number[i]);
+            sum = sum + number[i];
+        }
+        Console.WriteLine(sum);
+    }
+}
